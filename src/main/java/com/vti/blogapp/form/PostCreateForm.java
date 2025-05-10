0cb82e7de0ef7137459ee5f620e3cb29c5665f25
tranhienck52 <tranhienck52@gmail.com>
2025-05-11 00:33:27 +0900

    Add Post create form

---
+++ src/main/java/com/vti/blogapp/form/PostCreateForm.java
@@ -0,0 +1,15 @@
+package com.vti.blogapp.form;
+
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostCreateForm {
+    private String title;
+
+    private String description;
+
+    private String content;
+}
