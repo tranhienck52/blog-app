c9f5cf38aabfe2870f63f77165454106626de625
tranhienck52 <tranhienck52@gmail.com>
2025-05-15 22:08:06 +0900

    Add controller

--- src/main/java/com/vti/blogapp/form/PostCreateForm.java
+++ src/main/java/com/vti/blogapp/form/PostCreateForm.java
@@ -1,11 +1,13 @@
 package com.vti.blogapp.form;
 
 import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class PostCreateForm {
     private String title;
 
