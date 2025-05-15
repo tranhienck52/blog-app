faaa3e9ca111d719388ab04ee717ef077537d255
tranhienck52 <tranhienck52@gmail.com>
2025-05-15 23:14:39 +0900

    Add Find all post API

--- src/main/java/com/vti/blogapp/service/PostService.java
+++ src/main/java/com/vti/blogapp/service/PostService.java
@@ -3,8 +3,12 @@ package com.vti.blogapp.service;
 import com.vti.blogapp.dto.PostDto;
 import com.vti.blogapp.form.PostCreateForm;
 import com.vti.blogapp.mapper.PostMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface PostService {
+    Page<PostDto> findAll(Pageable pageable);
+
     PostDto create(PostCreateForm form);
 
 }
--- src/main/java/com/vti/blogapp/service/PostServiceImpl.java
+++ src/main/java/com/vti/blogapp/service/PostServiceImpl.java
@@ -1,17 +1,32 @@
 package com.vti.blogapp.service;
 
 import com.vti.blogapp.dto.PostDto;
+import com.vti.blogapp.entity.Post;
 import com.vti.blogapp.form.PostCreateForm;
 import com.vti.blogapp.mapper.PostMapper;
 import com.vti.blogapp.repository.PostRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class PostServiceImpl implements PostService{
 
     private final PostRepository postRepository;
+
+    @Override
+    public Page<PostDto> findAll(Pageable pageable) {
+        //Lambda
+        //method reference
+       return postRepository.findAll(pageable)
+                .map(PostMapper::map);
+    }
+
     @Override
     public PostDto create(PostCreateForm form) {
         var post = PostMapper.map(form);
