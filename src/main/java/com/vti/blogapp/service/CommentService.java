aaf0eb7c2a17947cb0f24a0997683930a2081b55
tranhienck52 <tranhienck52@gmail.com>
2025-05-17 16:26:32 +0900

    Add Create comment API

---
+++ src/main/java/com/vti/blogapp/service/CommentService.java
@@ -0,0 +1,10 @@
+package com.vti.blogapp.service;
+
+import com.vti.blogapp.dto.CommentDto;
+import com.vti.blogapp.entity.Comment;
+import com.vti.blogapp.entity.Post;
+import com.vti.blogapp.form.CommentCreateForm;
+
+public interface CommentService {
+    CommentDto create(CommentCreateForm form,Long postId);
+}
